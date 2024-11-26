e0f9860668716ae7ca6992246d84c34dd81c1d77
Ross <145527274+ross-ap@users.noreply.github.com>
2024-11-26 15:09:46 -0500

    GUI pushed to main, need to add functionality

---
+++ StressSolveApp/GUI/src/StressSolveGUI/gui/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appgui_FILE /home/ross/StressSolve/StressSolveApp/GUI/src/StressSolveGUI/gui/build/Desktop_Qt_6_8_0-Debug/appgui)
+set(__QT_DEPLOY_TARGET_appgui_TYPE EXECUTABLE)
